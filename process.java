ac8670458627d4e287afbd976d7590b9137929d5
Jamie Curran <jamie@jamie-curran.com>
2020-10-25 01:15:15 +0100

    Finished making lists

--- process.java
+++ process.java
@@ -1,7 +1,10 @@
 import roomdatas;
 
 public class process{
-    List<roomdatas.airQuality> _airQuality;
+    List<roomdatas.airQuality> _airQualityData;
+    List<roomdatas.trainData> _trainData;
+    List<roomdatas.populationLocation> _populationLocationData;
+    List<roomdatas.cardData> _cardData;
     public process(){
         
     }
